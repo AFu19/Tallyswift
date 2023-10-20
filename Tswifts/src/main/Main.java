13128c036149cd118ba11ebf396836e6728f4f06
NicholasFH <114159309+NicholasFH@users.noreply.github.com>
2023-10-20 22:50:26 +0700

    (AI GENERATED) membuat tambah kategori javafx
    membuat halaman tambah kategori menggunakan AI, jadi mohon diperiksa ulang takut ada kesalahan

--- Tswifts/src/main/Main.java
+++ Tswifts/src/main/Main.java
@@ -8,83 +8,67 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.ToolBar;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private TextField tfKategori, tfNamaBarang, tfHargaSatuan, tfStok;
-    private Button btnCancel, btnUbah;
+    private Label kategoriLabel = new Label("Kategori:");
+    private TextField kategoriTextField = new TextField();
+    private Label namaBarangLabel = new Label("Nama Barang:");
+    private TextField namaBarangTextField = new TextField();
+    private Label hargaSatuanLabel = new Label("Harga Satuan:");
+    private TextField hargaSatuanTextField = new TextField();
+    private Label stokLabel = new Label("Stok:");
+    private TextField stokTextField = new TextField();
+    private Button cancelButton = new Button("Cancel");
+    private Button tambahButton = new Button("Tambah");
 
     @Override
     public void start(Stage primaryStage) {
-        BorderPane root = new BorderPane();
+        VBox leftVBox = new VBox(kategoriLabel, kategoriTextField, namaBarangLabel, namaBarangTextField, hargaSatuanLabel, hargaSatuanTextField, stokLabel, stokTextField);
+        leftVBox.setPadding(new Insets(10));
+        leftVBox.setSpacing(10);
 
-        //Top menu
-        HBox topMenu = new HBox();
-        topMenu.setPadding(new Insets(5, 5, 5, 5));
-        topMenu.setSpacing(5);
-        topMenu.setAlignment(Pos.CENTER);
-        topMenu.getChildren().addAll(
-                new Button("Tambah Barang"),
-                new Button("Ubah Barang"),
-                new Button("Hapus Barang"),
-                new Button("Tambah Kategori")
+        HBox buttonHBox = new HBox(cancelButton, tambahButton);
+        buttonHBox.setAlignment(Pos.CENTER_RIGHT);
+        buttonHBox.setSpacing(10);
+
+        VBox centerVBox = new VBox(leftVBox, buttonHBox);
+        centerVBox.setSpacing(10);
+
+        Button transaksiPembelianButton = new Button("Transaksi Pembelian");
+        Button masterDataButton = new Button("Master Data");
+        HBox footerHBox = new HBox(transaksiPembelianButton, masterDataButton);
+        footerHBox.setAlignment(Pos.CENTER);
+        footerHBox.setSpacing(10);
+
+        // Create a ToolBar for the menu at the top
+        ToolBar topMenuToolBar = new ToolBar(
+                createSubMenuButton("Tambah Barang"),
+                createSubMenuButton("Ubah Barang"),
+                createSubMenuButton("Hapus Barang"),
+                createSubMenuButton("Tambah Kategori"),
+                createSubMenuButton("Ubah Kategori")
         );
-        root.setTop(topMenu);
 
-        //Center
-        GridPane center = new GridPane();
-        center.setPadding(new Insets(5, 5, 5, 5));
-        center.setHgap(5);
-        center.setVgap(5);
+        BorderPane root = new BorderPane();
+        root.setCenter(centerVBox);
+        root.setTop(topMenuToolBar); // Set the ToolBar as the top content
+        root.setBottom(footerHBox);
 
-        Label lblKategori = new Label("Kategori");
-        tfKategori = new TextField();
-        tfKategori.setPromptText("Masukkan kategori");
-
-        Label lblNamaBarang = new Label("Nama Barang");
-        tfNamaBarang = new TextField();
-        tfNamaBarang.setPromptText("Masukkan nama barang");
-
-        Label lblHargaSatuan = new Label("Harga Satuan");
-        tfHargaSatuan = new TextField();
-        tfHargaSatuan.setPromptText("Masukkan harga satuan");
-
-        Label lblStok = new Label("Stok");
-        tfStok = new TextField();
-        tfStok.setPromptText("Masukkan stok");
-
-        center.add(lblKategori, 0, 0);
-        center.add(tfKategori, 1, 0);
-        center.add(lblNamaBarang, 0, 1);
-        center.add(tfNamaBarang, 1, 1);
-        center.add(lblHargaSatuan, 0, 2);
-        center.add(tfHargaSatuan, 1, 2);
-        center.add(lblStok, 0, 3);
-        center.add(tfStok, 1, 3);
-
-        root.setCenter(center);
-
-        //Bottom
-        HBox bottom = new HBox();
-        bottom.setPadding(new Insets(5, 5, 5, 5));
-        bottom.setSpacing(5);
-        bottom.setAlignment(Pos.CENTER);
-
-        btnCancel = new Button("Cancel");
-        btnUbah = new Button("Ubah");
-
-        bottom.getChildren().addAll(btnCancel, btnUbah);
-
-        root.setBottom(bottom);
-
-        Scene scene = new Scene(root, 400, 300);
-
-        primaryStage.setTitle("Ubah Kategori");
+        Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Ecashier");
         primaryStage.show();
+    }
+
+    private Button createSubMenuButton(String text) {
+        Button button = new Button(text);
+        // Add any styling or event handling for the buttons here.
+        return button;
     }
 
     public static void main(String[] args) {
